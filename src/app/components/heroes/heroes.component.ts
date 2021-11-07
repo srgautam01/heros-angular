2e874d8d593d9ed1787a3899d62bba532c537216
sundar <sundar.gautam@selu.edu>
2021-11-07 15:39:25 -0600

    heroes component fixed with clean code

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -9,38 +9,41 @@ import { MessageService } from '../../services/message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
 
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
 }
 
+
 // export class HeroesComponent implements OnInit {
+
 //   heroes: Hero[] = [];
 
-//   constructor(private heroService: HeroService) { }
+//   selectedHero?: Hero;
 
-//   ngOnInit() {
+//   constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+//   ngOnInit(): void {
 //     this.getHeroes();
 //   }
-
+//   onSelect(hero: Hero): void {
+//     this.selectedHero = hero;
+//     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+//   }
 //   getHeroes(): void {
 //     this.heroService.getHeroes()
 //       .subscribe(heroes => this.heroes = heroes);
 //   }
 // }
+
